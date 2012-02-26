7bf961112ab2b9771bfa8c67867e700dec4d5726
Lars Glud <larshg@gmail.com>
2012-02-26 15:26:15 +0100

    Added save and load from DB on server
    Signed-off-by: Lars Glud <larshg@gmail.com>

--- CommonClientServerLib/MySQLDBHandler.cs
+++ CommonClientServerLib/MySQLDBHandler.cs
@@ -4,33 +4,70 @@ using System.Linq;
 using System.Text;
 using CommonClientServerLib.Messages;
 using MySql.Data.MySqlClient;
+using System.IO;
 
 namespace CommonClientServerLib
 {
     public class MySQLDBHandler
     {
-        public static List<SendMessage> GetSendMessages(long timestamp)
+        public static MySqlConnection GetConnection()
         {
-            string MyConString = "SERVER=localhost;" +
-                "DATABASE=mydatabase;" +
-                "UID=testuser;" +
-                "PASSWORD=testpassword;";
-            List<SendMessage> sendMessageList = new List<SendMessage>();
-            MySqlConnection connection = new MySqlConnection(MyConString);
+            //string MyConString = "SERVER=176.34.177.147:3306;" +
+            //    "DATABASE=ChatterDB;" +
+            //    "UID=ChatterServer;" +
+            //    "PASSWORD=BigBangTheory;";
+
+            string MyConString = "SERVER=127.0.0.1;" +
+                                 "DATABASE=ChatterDB;" +
+                                 "UID=ChatterServer;" +
+                                 "PASSWORD=BigBangTheory;";
+
+                MySqlConnection connection = new MySqlConnection(MyConString);
+
+                connection.Open();
+
+                return connection;
+        }
+
+        public static List<PublishMessage> GetStoredMessages(long timestamp)
+        {
+            MySqlConnection connection = GetConnection();
             MySqlCommand command = connection.CreateCommand();
             MySqlDataReader Reader;
-            command.CommandText = "select * from sendMessages";
-            connection.Open();
+
+            command.CommandText = "select * from publishmessages";
             Reader = command.ExecuteReader();
+
+            List<PublishMessage> sendMessageList = new List<PublishMessage>();
             while (Reader.Read())
             {
-                string thisrow = "";
-                for (int i = 0; i < Reader.FieldCount; i++)
-                    thisrow += Reader.GetValue(i).ToString() + ",";
+                PublishMessage msg = new PublishMessage();
+
+                msg.sender = Reader.GetString("sender");
+                msg.receiver = Reader.GetString("receiver");
+                msg.message = Reader.GetString("message");
+                msg.timeStamp = Reader.GetInt64("timeStamp");
+
+                sendMessageList.Add(msg);
             }
             connection.Close();
 
             return sendMessageList;
+        }
+
+        public static int SavePublisMsg(PublishMessage msg)
+        {
+            MySqlConnection connection = GetConnection();
+            MySqlCommand command = connection.CreateCommand();
+
+            command.CommandText = @"INSERT INTO publishmessages (sender, receiver, message, timeStamp) VALUES(@sender,@receiver,@message,@timeStamp);";
+
+            command.Parameters.AddWithValue("@sender", msg.sender);
+            command.Parameters.AddWithValue("@receiver", msg.receiver);
+            command.Parameters.AddWithValue("@message", msg.message);
+            command.Parameters.AddWithValue("@timeStamp", msg.timeStamp);
+
+            return command.ExecuteNonQuery();
         }
     }
 }
