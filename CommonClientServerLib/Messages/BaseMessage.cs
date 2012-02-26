2d5299a4b7b50649bc63bacac0871b9839af85c5
Lars Glud <larshg@gmail.com>
2012-02-26 15:27:11 +0100

    Made GetNewMessages public. Added so that timestamp is filled with current time on packet creation...

--- CommonClientServerLib/Messages/BaseMessage.cs
+++ CommonClientServerLib/Messages/BaseMessage.cs
@@ -5,9 +5,19 @@ using System.Text;
 
 namespace CommonClientServerLib.Messages
 {
-    public abstract class BaseMessage
+    public class BaseMessage
     {
+        public BaseMessage()
+        {
+            timeStamp = GetTimeStamp();
+        }
         public string errorMessage { get; set; }
         public long timeStamp { get; set; }
+
+
+        public static long GetTimeStamp()
+        {
+            return (long)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalMilliseconds;
+        }
     }
 }
--- CommonClientServerLib/Messages/GetNewMessages.cs
+++ CommonClientServerLib/Messages/GetNewMessages.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace CommonClientServerLib.Messages
 {
-    class GetNewMessages : BaseMessage, IComMessage
+    public class GetNewMessages : BaseMessage, IComMessage
     {
         public string receiver { get; set; }
         public long lastSeenTimeStamp { get; set; }
